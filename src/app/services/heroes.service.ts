da860b1ce0784641d2becb1854df2a763cb96b3d
Daniel M Sanchez <dsanchez@existaya.com>
2018-09-11 17:26:50 -0500

    se creo funcion en el servicio de heroes que me busca por nombre

--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -65,6 +65,21 @@ export class HeroesService {
     getHeroe( idx: string ): Heroe {
         return this.heroes[idx];
     }
+
+    findHeroes( termino: string ) {
+        const heroesArr: Heroe[] = [];
+        termino = termino.toLowerCase();
+
+        for ( const heroe of this.heroes ) {
+            const nombre = heroe.nombre.toLowerCase();
+
+            if (nombre.indexOf(termino) >= 0) {
+                heroesArr.push(heroe);
+            }
+        }
+
+        return heroesArr;
+    }
 }
 
 export interface Heroe {
