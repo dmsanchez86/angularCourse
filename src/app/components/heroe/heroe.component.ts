449c4ca1bbd1d3c9c655005341a5bc66a28b483b
Daniel M Sanchez <dsanchez@existaya.com>
2018-09-11 16:43:14 -0500

    se agrego codigo necesario para obtener toda la informacion de mi heroe seleccionado

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,12 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService, Heroe } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html'
 })
 export class HeroeComponent implements OnInit {
+  heroe: Heroe;
 
-  constructor() { }
+  constructor(
+      private _activateRoute: ActivatedRoute ,
+      private _heroesService: HeroesService
+    ) {
+    this._activateRoute.params.subscribe( params => {
+      this.heroe = this._heroesService.getHeroe(params['id']);
+    });
+  }
 
   ngOnInit() {
   }
