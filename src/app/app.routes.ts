a65da70ab5308ad94bca544c00b880796eea122d
Daniel M Sanchez <dsanchez@existaya.com>
2018-09-11 15:19:37 -0500

    se agrego la ruta de heroes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { Component } from '@angular/core';
 
 // componentes
-import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
@@ -10,7 +9,8 @@ import { HeroesComponent } from './components/heroes/heroes.component';
 const APP_ROUTES: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent },
-    { path: '**', pathMatch: 'full', redirectTo: 'home' },
+    { path: 'heroes', component: HeroesComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
